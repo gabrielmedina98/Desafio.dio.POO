57e0ead3d0f300568a45644723e14dd9674a283d
gabrielmedina98 <gabriel_r1998@hotmail.com>
2022-03-31 17:53:56 -0300

    Mentoria e Curso

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import br.com.dio.desafio.domínio.Curso;
+import br.com.dio.desafio.domínio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Curso curso1 = new Curso();
+        curso1.setTitulo("cuso java");
+        curso1.setDescricao("descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("cuso js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria =  new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
